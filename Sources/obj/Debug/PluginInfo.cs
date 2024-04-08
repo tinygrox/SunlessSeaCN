8ae8c7b84d695f083bd461f2cd6cc2c9743122f3
Tinygrox <tinygrox@gmail.com>
2024-04-08 20:03:17 +0800

    规整文件

---
+++ Sources/obj/Debug/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SSTranslator
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SSTranslator";
+        public const string PLUGIN_NAME = "SSTranslator";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ Sources/obj/Release/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SSTranslator
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SunlessSeaChineseTranslation";
+        public const string PLUGIN_NAME = "SunlessSeaChineseTranslation";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
