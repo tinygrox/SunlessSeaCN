ce33fc299cab8fc8bc98d1544541a779c65b3bfa
Tinygrox <tinygrox@gmail.com>
2024-04-09 01:52:50 +0800

    #

--- Sources/obj/Debug/PluginInfo.cs
+++ Sources/obj/Debug/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace SSTranslator
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "SSTranslator";
-        public const string PLUGIN_NAME = "SSTranslator";
+        public const string PLUGIN_GUID = "SunlessSeaChineseTranslation";
+        public const string PLUGIN_NAME = "SunlessSeaChineseTranslation";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
